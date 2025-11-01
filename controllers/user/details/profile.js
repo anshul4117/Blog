f99eee521a874bfec9621ca35c72a91a8f6bc979
Anshul <anshul41171@gmail.com>
2025-11-01 21:55:29 +0530

    adding the user profile file to fetching user details

---
+++ controllers/user/details/profile.js
@@ -0,0 +1,25 @@
+import User from '../../../models/user.js';
+
+const userProfile = async (req, res) => {
+    try {
+        const userId = req.user.userId;
+        const getProfile = await User.findById(userId);
+        if (!getProfile) {
+            return res.status(404)
+                .json({
+                    message: "Profile Unavailable"
+                })
+        }
+        return res.status(200)
+            .json({
+                message: "fetched Profile",
+                getProfile
+            })
+    } catch (error) {
+        return res.status(500)
+            .json({
+                message: "Internal Server Error"
+            })
+    }
+}
+export { userProfile }
--- routes/auth.js
+++ routes/auth.js
@@ -4,11 +4,13 @@ import loginUser from '../controllers/user/loginUser.js';
 import allUsers from '../controllers/user/getAllUsers.js';
 import auth from '../middleware/auth.js';
 import updateProfile from '../controllers/user/updateProfile.js';
+import { userProfile } from '../controllers/user/details/profile.js';
 const router = express.Router();
 
 router.post('/create', createUser);
 router.post('/login', loginUser);
-router.get('/allUsers',auth, allUsers);
-router.patch('/edit-user/:id',auth, updateProfile);
+router.get('/allUsers', auth, allUsers);
+router.patch('/edit-user/:id', auth, updateProfile);
+router.get('/profile', auth, userProfile);
 
 export default router;  
